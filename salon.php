9575720a660ab2782cc5f8ad0e3d40b0da8e718f
alexei <soloveyalexey3@gmail.com>
2019-04-13 18:44:56 +0300

    added slider

--- salon.php
+++ salon.php
@@ -26,107 +26,51 @@
         </div>
     </div>
 
-	<div class="salon_title" style="background: gray; padding: 20px; margin:10px;">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="main_title left_text">
-                        Тут будет cлайдер                   
-                    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="custome-slider">
+                  <div class="slick-slide">
+                    <a href="#" class="js-fancy">
+                        <img src="img/salon-slide.jpg" class="img-responsive" alt="">
+                    </a>
+                  </div>
+                  <div class="slick-slide">
+                    <a href="#" class="js-fancy">
+                        <img src="img/salon-slide.jpg" class="img-responsive" alt="">
+                    </a>
+                  </div>
+                  <div class="slick-slide">
+                    <a href="#" class="js-fancy">
+                        <img src="img/salon-slide.jpg" class="img-responsive" alt="">
+                    </a>
+                  </div>
+                  <div class="slick-slide">
+                    <a href="#" class="js-fancy">
+                        <img src="img/salon-slide.jpg" class="img-responsive" alt="">
+                    </a>
+                  </div>
+                  <div class="slick-slide">
+                    <a href="#" class="js-fancy">
+                        <img src="img/salon-slide.jpg" class="img-responsive" alt="">
+                    </a>
+                  </div>
+                  <div class="slick-slide">
+                    <a href="#" class="js-fancy">
+                        <img src="img/salon-slide.jpg" class="img-responsive" alt="">
+                    </a>
+                  </div>
+                  <div class="slick-slide">
+                    <a href="#" class="js-fancy">
+                        <img src="img/salon-slide.jpg" class="img-responsive" alt="">
+                    </a>
+                  </div>
                 </div>
             </div>
         </div>
     </div>
 
-
-	<div class="salon_slider">
-        <div class="container">
-            <div class="row">
-                <div class="js_salon_slider slick-initialized slick-slider" style="display: block;"><div class="slick_prev slick-arrow"></div>
-                    
-                                            <div class="slick-list draggable"><div class="slick-track" style="opacity: 1; width: 4860px; transform: translate3d(-1080px, 0px, 0px);"><div class="slick-slide slick-cloned" data-slick-index="-4" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/2241321d0f99a470a04f94673b0ea961.0.jpg?code=MjkzMi4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/2241321d0f99a470a04f94673b0ea961.336_220.jpg,qcode=MjkzMi4zMzZfMjIw.pagespeed.ce.QkPXXdvF6X.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="784274157" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="-3" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/8a560457c1e96616cfab04fa2cfd0a92.0.jpg?code=MjkzMy4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/8a560457c1e96616cfab04fa2cfd0a92.336_220.jpg,qcode=MjkzMy4zMzZfMjIw.pagespeed.ce.wkVShMpnj0.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="2219529935" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="-2" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/04d659a7aa026840e738f157754ab38e.0.jpg?code=MjkzNC4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/04d659a7aa026840e738f157754ab38e.336_220.jpg,qcode=MjkzNC4zMzZfMjIw.pagespeed.ce.DyHxF_FbBT.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="3393825474" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="-1" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/cf06f899b1660a5b73e0227c4274a9cc.0.jpg?code=MjkzNS4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/cf06f899b1660a5b73e0227c4274a9cc.336_220.jpg,qcode=MjkzNS4zMzZfMjIw.pagespeed.ce.IPx6eLRDpo.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="3667855915" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" tabindex="0" style="width: 256px;">
-                            <a href="/uploads/pages/45/abb74c8cf3d6fd82835b683c71106bb2.0.jpg?code=MjkyOS4w" class="js-fancy" rel="gallery-salon" tabindex="0">
-                                <img src="/uploads/pages/45/abb74c8cf3d6fd82835b683c71106bb2.336_220.jpg,qcode=MjkyOS4zMzZfMjIw.pagespeed.ce.fs-c6GBPLv.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="4248424957" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-active" data-slick-index="1" aria-hidden="false" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/199038e99bbbb80f16297acf0a2e1d27.0.jpg?code=MjkzMC4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/199038e99bbbb80f16297acf0a2e1d27.336_220.jpg,qcode=MjkzMC4zMzZfMjIw.pagespeed.ce.1jr8DJmILo.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="192536172" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-active" data-slick-index="2" aria-hidden="false" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/10258599d156f3903e7c232305a8f73b.0.jpg?code=MjkzMS4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/10258599d156f3903e7c232305a8f73b.336_220.jpg,qcode=MjkzMS4zMzZfMjIw.pagespeed.ce.jMqeI0Iksm.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="3697921739" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-active" data-slick-index="3" aria-hidden="false" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/2241321d0f99a470a04f94673b0ea961.0.jpg?code=MjkzMi4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/2241321d0f99a470a04f94673b0ea961.336_220.jpg,qcode=MjkzMi4zMzZfMjIw.pagespeed.ce.QkPXXdvF6X.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="784274157" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide" data-slick-index="4" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/8a560457c1e96616cfab04fa2cfd0a92.0.jpg?code=MjkzMy4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/8a560457c1e96616cfab04fa2cfd0a92.336_220.jpg,qcode=MjkzMy4zMzZfMjIw.pagespeed.ce.wkVShMpnj0.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="2219529935" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide" data-slick-index="5" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/04d659a7aa026840e738f157754ab38e.0.jpg?code=MjkzNC4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/04d659a7aa026840e738f157754ab38e.336_220.jpg,qcode=MjkzNC4zMzZfMjIw.pagespeed.ce.DyHxF_FbBT.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="3393825474" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide" data-slick-index="6" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/cf06f899b1660a5b73e0227c4274a9cc.0.jpg?code=MjkzNS4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/cf06f899b1660a5b73e0227c4274a9cc.336_220.jpg,qcode=MjkzNS4zMzZfMjIw.pagespeed.ce.IPx6eLRDpo.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="3667855915" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="7" aria-hidden="true" tabindex="0" style="width: 256px;">
-                            <a href="/uploads/pages/45/abb74c8cf3d6fd82835b683c71106bb2.0.jpg?code=MjkyOS4w" class="js-fancy" rel="gallery-salon" tabindex="0">
-                                <img src="/uploads/pages/45/abb74c8cf3d6fd82835b683c71106bb2.336_220.jpg,qcode=MjkyOS4zMzZfMjIw.pagespeed.ce.fs-c6GBPLv.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="4248424957" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="8" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/199038e99bbbb80f16297acf0a2e1d27.0.jpg?code=MjkzMC4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/199038e99bbbb80f16297acf0a2e1d27.336_220.jpg,qcode=MjkzMC4zMzZfMjIw.pagespeed.ce.1jr8DJmILo.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="192536172" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="9" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/10258599d156f3903e7c232305a8f73b.0.jpg?code=MjkzMS4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/10258599d156f3903e7c232305a8f73b.336_220.jpg,qcode=MjkzMS4zMzZfMjIw.pagespeed.ce.jMqeI0Iksm.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="3697921739" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="10" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/2241321d0f99a470a04f94673b0ea961.0.jpg?code=MjkzMi4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/2241321d0f99a470a04f94673b0ea961.336_220.jpg,qcode=MjkzMi4zMzZfMjIw.pagespeed.ce.QkPXXdvF6X.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="784274157" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="11" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/8a560457c1e96616cfab04fa2cfd0a92.0.jpg?code=MjkzMy4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/8a560457c1e96616cfab04fa2cfd0a92.336_220.jpg,qcode=MjkzMy4zMzZfMjIw.pagespeed.ce.wkVShMpnj0.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="2219529935" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="12" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/04d659a7aa026840e738f157754ab38e.0.jpg?code=MjkzNC4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/04d659a7aa026840e738f157754ab38e.336_220.jpg,qcode=MjkzNC4zMzZfMjIw.pagespeed.ce.DyHxF_FbBT.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="3393825474" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div><div class="slick-slide slick-cloned" data-slick-index="13" aria-hidden="true" tabindex="-1" style="width: 256px;">
-                            <a href="/uploads/pages/45/cf06f899b1660a5b73e0227c4274a9cc.0.jpg?code=MjkzNS4w" class="js-fancy" rel="gallery-salon" tabindex="-1">
-                                <img src="/uploads/pages/45/cf06f899b1660a5b73e0227c4274a9cc.336_220.jpg,qcode=MjkzNS4zMzZfMjIw.pagespeed.ce.IPx6eLRDpo.jpg" class="img-responsive" alt="" data-pagespeed-url-hash="3667855915" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                            </a>
-                        </div></div></div>
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                    <div class="slick_next slick-arrow"></div></div>
-            </div>
-        </div>
-    </div>
 
 
 
