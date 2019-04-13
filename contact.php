868eb52c56bcaf891227357c9af6cd1da646a30f
Nikolay Tavolzhanskiy <tavolzahaznskiy@gmail.com>
2019-04-13 18:40:31 +0300

    my contact

---
+++ contact.php
@@ -0,0 +1,221 @@
+<?php include('header.php'); ?>
+<header class="header">
+	<div class="header_bottom_line">
+		<div class="container">
+		    <div class="row">
+		        <ul class="">
+		            <li>
+			            <a href="/vhodnie-dveri" title="Входные двери" class="">
+			            Входные двери</a>
+			        </li>
+
+			        <li>
+			            <a href="/mezhkomnatnye-dveri" title="Межкомнатные двери" class="">Межкомнатные двери</a>
+			        </li>
+
+			        <li>
+			            <a href="/kuhni" title="Кухни" class="">Кухни</a>
+			        </li>
+
+			        <li>
+			            <a href="/balkon" title="Балконы" class="">Балконы</a>
+			        </li>
+
+			        <li>
+			            <a href="/cottage" title="Остекление коттеджей" class="">Остекление коттеджей</a>
+			        </li>
+			    </ul>
+		    </div>
+		</div>
+    </div>
+</header>
+
+
+<div class="content">
+
+    <div class="breadcrumb_wrapp">
+        <div class="container">
+            <ol class="breadcrumb">
+				<li >
+					<a itemprop="item" href="/" class="active">
+						<span itemprop="name">Главная</span>
+					</a>  
+				</li>  
+				<li class="active">
+					<span itemprop="name">Контакты</span>
+				</li>
+			</ol>
+		</div>
+    </div>
+    
+    <!--content-->
+
+    <section class="contacts">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-sm-12">
+                    <div class="main_title left_text">
+                        Контакты                    
+                    </div>
+                </div>
+
+                <!--List of contacts-->
+                <div class="col-md-3 col-sm-4 contacts_wrapp">
+                    <div class="contacts_wrapp_item">
+                        <div class="title">
+                            <span>
+                            	<img src="img/icon/f2f98be7e06ca6ad2495ba0292f1ea4f.0.png,qcode=MjkzNi4w.pagespeed.ce.GXYq03nmXe.png" class="img-responsive" alt="">
+                            </span>
+                            Адрес                            
+                        </div>
+                        <div class="desc">
+                            Киев, ул.Вербовая, 24, 2 этаж     
+                        </div>
+                    </div>
+
+                    <div class="contacts_wrapp_item">
+                        <div class="title">
+                            <span>
+                            	<img src="img/icon/c4b3a96857410ab34767f4e5fe08dccc.0.png,qcode=MjkzNy4w.pagespeed.ce.FqVFToCKCL.png" class="img-responsive" alt="">
+                            </span>
+                            График работы                            
+                        </div>
+                        <div class="desc">
+                            ПН-СБ 9:00-19:00<br>
+						    Воскресенье выходной                            
+						</div>
+                    </div>
+
+                    <div class="contacts_wrapp_item">
+                        <div class="title">
+                            <span>
+                            	<img src="img/icon/345a12ef99e6a3ead457527e10973fb6.0.png,qcode=MjkzOC4w.pagespeed.ce.We68WWYLvq.png" class="img-responsive" alt="">
+                            </span>
+                            Электронная почта                            
+                        </div>
+
+                        <div class="desc">
+                            info@wid.ua                            
+                        </div>
+                    </div>
+
+                    <div class="contacts_wrapp_item">
+                        <div class="title">
+                            <span>
+                            	<img class="img-responsive" alt=""  src="img/icon/9102caf0c2b54a23292248ee964f5dea.0.png,qcode=MjkzOS4w.pagespeed.ce.XBZP5BmmkU.png">
+                            </span>
+                            Телефон                            
+                        </div>
+                        <div class="desc">
+                            +38 (044) 228 98 26<br>
+							+38 (067) 508 52 21                            
+						</div>
+                    </div>
+                </div>
+
+                <!--Map-->
+                <div class="col-md-9 col-sm-8">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1269.3047445731204!2d30.497762404346528!3d50.48561329511868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cdfd0c56f15f%3A0xb6a1b1d1a7f1a602!2z0YPQuy4g0JLQtdGA0LHQvtCy0LDRjywgMjQsINCa0LjQtdCyLCAwMjAwMA!5e0!3m2!1sru!2sua!4v1535627113770" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+                    <style type="text/css">
+                        iframe {height: 600px !important;}
+                    </style>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<footer class="footer section_border">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <a href="#">
+                	<img src="img/logo_190_67.png.pagespeed.ce.BS15qWulYk.png" class="img-responsive logo_footer" alt="" >
+                </a>
+            </div>
+
+            <div class="col-sm-4 footer_class">
+                <ul class="footer_list">
+                    <li>
+		                <a href="/" title="Главная" class="">Главная</a>
+		            </li>
+
+		            <li>
+		                <a href="/blog" title="Блог" class="">Блог</a>
+		            </li>
+
+		            <li>
+		                <a href="/page/about" title="О нас" class="">О нас</a>
+		            </li>
+
+		            <li>
+		                <a href="/page/rassrochka" title="Рассрочка" class="">Рассрочка</a>
+		            </li>
+
+		            <li>
+		                <a href="/page/salon" title="Салон" class="">Салон</a>
+		            </li>
+
+		            <li>
+		                <a href="/page/contacts" title="Контакты" class="current">Контакты</a>
+		            </li>
+
+		            <li>
+		                <a href="/page/sitemap" title="Карта сайта" class="">Карта сайта</a>
+		            </li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-md-offset-1 col-sm-4 phone_header phone_header_footer">
+                <a href="tel:+380442289886" class="phone">+38 (044) 228-98-86</a>
+
+                <button class="button button_red button_ico" data-toggle="modal" data-target=".modal_call" data-title="Заказать звонок" data-subject="Заказать звонок с главного сайта">
+                    <span>
+                        Заказать звонок
+                    </span>
+                </button>
+
+                <div class="address">
+                    Киев, ул.Вербовая , 24, 2 этаж                
+                </div>
+
+                <div class="mail">
+                    <a href="mailto:info@wid.ua">info@wid.ua</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<div class="footer_line">
+    <div class="container">
+        <div class="row row_flex">
+            <div class="col-sm-4">
+                <div class="soc">
+                    <a href="#" target="_blank" rel="nofollow">
+                    	<img class="img-responsive" alt="" src="img/icon/fb.png.pagespeed.ce.dtdQx8G5LR.png" >
+                    </a>
+
+                    <a href="#" target="_blank" rel="nofollow">
+                    	<img class="img-responsive" alt="" src="img/icon/inst.png.pagespeed.ce.HNbLBJ8m9E.png">
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-sm-4 copy">
+                Все права защищены. 2008-2019 Компания "Ваш Вид"
+            </div>
+            <div class="col-sm-4">
+                <div class="studio">
+                    Сайт разработан<br>
+                    в компании <span class="name">WEBRA</span>
+                    <a href="#" rel="nofollow" target="_blank" class="link"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('footer.php'); ?>
